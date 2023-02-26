f2770bb3e280eb2a9d4417d891816247bd10ee07
天夜Yoku <mamaitoto0206@gmail.com>
2023-02-26 22:15:23 +0800

    Added CraftBukkit_1_17_R1 Module

---
+++ CraftBukkit_1_17_R1/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("com.github.johnrengelman.shadow")
+    id("io.github.rancraftplayz.remapper")
+}
+
+dependencies {
+
+    compileOnly(group = "org.spigotmc", name = "spigot", version = "1.17.1-R0.1-SNAPSHOT", classifier = "remapped-mojang")
+
+    implementation(project(":API"))
+
+    remapLib("org.spigotmc:spigot:1.17.1-R0.1-SNAPSHOT:remapped-mojang")
+    accessWidenerLib("org.spigotmc:spigot:1.17.1-R0.1-SNAPSHOT:remapped-mojang")
+}
+
+spigot {
+    version = "1.17.1"
+}
+
+tasks {
+
+    shadowJar {
+
+        archiveBaseName.set("${project.name}-${project.version}")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+
+        finalizedBy(remapJar)
+
+    }
+
+    remapJar {
+
+        dependsOn(shadowJar)
+
+    }
+
+    build {
+
+        dependsOn(remapJar)
+
+    }
+
+}
---
+++ CraftBukkit_1_17_R1/src/main/kotlin/me/yoku/craftbukkit_1_17_r1/nbt/CodeNBTList.kt
@@ -0,0 +1,96 @@
+package me.yoku.craftbukkit_1_17_r1.nbt
+
+import me.yoku.api.CodeNBTType
+import me.yoku.api.NBTIterator
+import me.yoku.api.NBTList
+import me.yoku.api.NBTType
+import net.minecraft.nbt.ListTag
+
+@Suppress("UNCHECKED_CAST", "unused")
+open class CodeNBTList<T> : NBTList<T> {
+
+    private var tag: ListTag
+
+    constructor() {
+        this.tag = ListTag()
+    }
+
+    constructor(tag: ListTag) {
+        this.tag = tag
+    }
+
+    fun getNMSTag() : ListTag {
+        return this.tag
+    }
+
+    override fun getType(): NBTType {
+        return CodeNBTType.fromNMS(this.tag.id.toInt())
+    }
+
+    override fun getOwnType(): NBTType {
+        return CodeNBTType.fromNMS(this.tag.elementType.toInt())
+    }
+
+    override fun toList() : List<T> {
+
+        val list = mutableListOf<T>()
+
+        for (i in 0 until nbtSize()) list.add(i, get(i))
+
+
+        return list
+    }
+
+    override val size: Int
+        get() = nbtSize()
+
+    override fun iterator(): Iterator<T> {
+        return NBTIterator(this)
+    }
+
+    override fun toArray(): Array<Any?> {
+        return arrayOf(toList())
+    }
+
+    inline fun <reified C> toTypeArray() : Array<C> {
+        return (toList() as List<C>).toTypedArray()
+    }
+
+    override fun nbtSize(): Int {
+        return this.tag.size
+    }
+
+    override fun isEmpty(): Boolean {
+        return this.tag.isEmpty()
+    }
+
+    override fun contains(element: T): Boolean {
+
+        val m = CodeNBT.toTag(element) ?: false
+
+        for (i in 0 until nbtSize()) if (get(i)?.equals(m) == true) return true
+
+        return false
+    }
+
+    override fun get(index: Int) : T { return CodeNBT.getData(tag[index]) as T }
+
+    override fun add(index: Int, value: Any) { getNMSTag()[index] = CodeNBT.toTag(value) }
+
+    override fun add(value: T) : Boolean { return getNMSTag().add(CodeNBT.toTag(value)) }
+
+    override fun set(index: Int, value: Any) : T { return CodeNBT.getData(getNMSTag().set(index, CodeNBT.toTag(value))) as T }
+
+    override fun remove(index: Int) : T { return CodeNBT.getData(getNMSTag().removeAt(index)) as T }
+
+    override fun clear() { getNMSTag().clear() }
+
+    override fun clone(): NBTList<T> { return CodeNBTList(tag.clone()) }
+
+    override fun toString() : String { return tag.asString }
+
+    override fun hashCode(): Int { return getNMSTag().hashCode() }
+
+    override fun equals(other: Any?): Boolean { return other is NBTList<*> && getNMSTag() == (other as CodeNBTList<*>).getNMSTag()
+    }
+}
